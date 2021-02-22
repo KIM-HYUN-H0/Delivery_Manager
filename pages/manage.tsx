deac27724a65a5d1ead7ee3c9b6c4763014ac1ad
KIM-HYUN-H0 <cardbye@naver.com>
2021-02-22 19:12:35 +0900

    axios lambda

--- pages/manage.tsx
+++ pages/manage.tsx
@@ -1,7 +1,15 @@
 import React, {useEffect} from 'react';
 import Link from "next/link";
+import axios from 'axios';
 
 const manage = () => {
+
+    useEffect(() => {
+        axios.get('https://pb5twplevd.execute-api.ap-northeast-2.amazonaws.com/Hello-lambda')
+        .then((data) => {
+            console.log(data);
+        })
+    })
    return(
        <>
        manage
